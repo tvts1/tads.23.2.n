c33fa3488c90f8fccaa30ab6e6db0a7fbee41623
Tássio_Tristão <144722913+tvts1@users.noreply.github.com>
2023-11-30 23:53:57 +0000

    [feat]: created sum_two_numbers()

---
+++ sum_two_numbers.py
@@ -0,0 +1,20 @@
+def sum_two_number(
+    a:float,
+    b:float = 5,
+) -> float:
+    """ Sum two values. 
+
+    Take two variabels and sum.
+
+    Args:
+        a (float): First number.
+        b (float, optional): Second number. Defaults to 5.
+
+    Returns:
+        float: The sum of 'a' + 'b'
+    """
+
+
+    sum = a + b
+
+    return sum
